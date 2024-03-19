4d9dce2ba5053371f97e751ae33e4704992a4ee7
Tashila Fernando <tashila.dev@gmail.com>
2024-03-19 15:41:53 +0530

    refactor: add in server

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"net/http"
 )
 
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from a Go program"))
+}
+
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from a Go program"))
-	})
-	err := http.ListenAndServe(":3333", nil)
+	server := http.NewServeMux()
+	server.HandleFunc("/hello", handleHello)
+
+	err := http.ListenAndServe(":3333", server)
 	if err == nil {
 		fmt.Println("Error while opening the server")
 	}
